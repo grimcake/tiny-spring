46dd590d79399cb02af221c513898cc8ba914cca
grimcake <994459124@qq.com>
2021-08-04 19:43:05 +0800

    创建工程

---
+++ src/test/java/com/grimcake/springframework/test/ApiTest.java
@@ -0,0 +1,15 @@
+package com.grimcake.springframework.test;
+
+import com.grimcake.springframework.BeanDefinition;
+import com.grimcake.springframework.BeanFactory;
+import org.junit.Test;
+
+public class ApiTest {
+    @Test
+    public void test_BeanFactory() {
+        BeanFactory beanFactory = new BeanFactory();
+
+        BeanDefinition beanDefinition = new BeanDefinition()
+    }
+}
+
